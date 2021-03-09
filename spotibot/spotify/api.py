478c34ff7927658a56977b5808c18f2c17efa916
fedecalendino <fede@calendino.com>
2021-03-09 22:23:11 +0100

    Fix cache location

--- spotibot/spotify/api.py
+++ spotibot/spotify/api.py
@@ -1,8 +1,9 @@
 import logging
 from typing import Iterable
+from pathlib import Path
 
 from django.conf import settings
-from spotipy import Spotify
+from spotipy import Spotify, CacheFileHandler
 from spotipy.oauth2 import SpotifyOAuth
 
 from . import util
@@ -17,6 +18,7 @@ client = Spotify(
         client_secret=settings.SPOTIFY["CLIENT_SECRET"],
         scope="playlist-modify-public,playlist-modify-private,user-library-read,user-read-recently-played",
         redirect_uri="https://localhost:8080",
+        cache_handler=CacheFileHandler(cache_path=str(Path.home() / ".spotify"))
     )
 )
 
