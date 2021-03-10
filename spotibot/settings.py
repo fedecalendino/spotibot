6702ddfc526f6da63c6648e47dbf8270cbf400a5
fedecalendino <fede@calendino.com>
2021-03-11 00:29:48 +0100

    Add pagination

--- spotibot/settings.py
+++ spotibot/settings.py
@@ -138,6 +138,11 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'MAX_PAGE_SIZE': 50,
+}
 
 # Internationalization ========================================================
 
